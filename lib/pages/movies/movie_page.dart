1c4f86da81d3c0fb4ddb918ff9a4122c68500fbd
rlecheta <rlecheta@livetouch.com.br>
2019-06-11 12:00:57 -0300

    movies

--- lib/pages/movies/movie_page.dart
+++ lib/pages/movies/movie_page.dart
@@ -1,10 +1,9 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_movies_udemy/pages/movies/movie.dart';
 
 class MoviePage extends StatefulWidget {
-
   final Movie movie;
+
   MoviePage(this.movie);
 
   @override
@@ -14,6 +13,8 @@ class MoviePage extends StatefulWidget {
 class _MoviePageState extends State<MoviePage> {
   final favoritado = false;
 
+  Movie get movie => widget.movie;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,51 +23,135 @@ class _MoviePageState extends State<MoviePage> {
   }
 
   _body() {
-    return CustomScrollView(
-      slivers: <Widget>[
-        SliverAppBar(
-          backgroundColor: Colors.deepOrange,
-          expandedHeight: 350,
-          pinned: false,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.favorite,
-                size: 34,
-                color: favoritado ? Colors.red: Colors.white,
-              ),
-              onPressed: () {
-                _onClickFavoritar();
-              },
+    return CustomScrollView(slivers: <Widget>[
+      SliverAppBar(
+        backgroundColor: Colors.deepOrange,
+        expandedHeight: 350,
+        pinned: false,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.favorite,
+              size: 34,
+              color: favoritado ? Colors.red : Colors.white,
             ),
-          ],
-          flexibleSpace: FlexibleSpaceBar(
-            centerTitle: false,
-            title: Text(widget.movie.title),
-            background: Container(
-              child: Image.network(
-                widget.movie.urlFoto,
-                fit: BoxFit.cover,
-              ),
+            onPressed: () {
+              _onClickFavoritar();
+            },
+          ),
+        ],
+        flexibleSpace: FlexibleSpaceBar(
+          centerTitle: false,
+          title: Text(widget.movie.title),
+          background: Container(
+            child: Image.network(
+              widget.movie.urlFoto,
+              fit: BoxFit.cover,
             ),
           ),
         ),
-        SliverList(
-          delegate: SliverChildListDelegate(
-            [
-              Container(
-                padding: EdgeInsets.all(20),
-                child: Text(
-                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                  style: TextStyle(fontSize: 28),
+      ),
+      SliverList(delegate: _sliver())
+    ]);
+  }
+
+  _sliver() {
+    return SliverChildListDelegate(
+      [
+        Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  _card(Icons.favorite, movie.vote_count),
+                  _card(Icons.star, movie.vote_average),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  _card(Icons.movie, movie.popularity),
+                  _card(Icons.date_range, movie.release_date),
+                ],
+              )
+            ],
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(26),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              colors: [Colors.red, Colors.deepOrange],
+            ),
+          ),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.description,
+                    color: Colors.white,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    "Descrição",
+                    style: TextStyle(
+                      fontSize: 26,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 26,
+              ),
+              Text(
+                movie.overview,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                  letterSpacing: 1,
                 ),
+              ),
+              SizedBox(
+                height: 26,
               ),
             ],
           ),
-        )
+        ),
       ],
+    );
+  }
+
+  _card(IconData icone, dynamic texto) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        padding: EdgeInsets.only(top: 30, bottom: 30, left: 15, right: 15),
+        margin: EdgeInsets.all(15),
+        decoration: new BoxDecoration(
+          border: Border.all(width: 1.0, color: Colors.grey),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          children: <Widget>[
+            Icon(
+              icone,
+              color: Colors.deepOrange,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              "$texto",
+              style: TextStyle(fontSize: 18, color: Colors.black),
+            )
+          ],
+        ),
+      ),
     );
   }
 
