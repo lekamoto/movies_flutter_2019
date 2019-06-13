f8eac4eba031138b99218846f4bba3613fa4b37f
rlecheta <rlecheta@livetouch.com.br>
2019-06-13 11:48:35 -0300

    Flare

--- lib/pages/movies/movie_page.dart
+++ lib/pages/movies/movie_page.dart
@@ -1,3 +1,4 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_movies_udemy/pages/movies/movie.dart';
 import 'package:flutter_movies_udemy/pages/movies/movie_bloc.dart';
@@ -37,22 +38,7 @@ class _MoviePageState extends State<MoviePage> {
         expandedHeight: 350,
         pinned: false,
         actions: <Widget>[
-          IconButton(
-            icon: StreamBuilder<bool>(
-              initialData: false,
-              stream: _bloc.getFavoritos,
-              builder: (context, snapshot) {
-                return Icon(
-                  Icons.favorite,
-                  size: 34,
-                  color: snapshot.data ? Colors.red : Colors.white,
-                );
-              },
-            ),
-            onPressed: () {
-              _onClickFavoritar();
-            },
-          ),
+          iconFavorito()
         ],
         flexibleSpace: FlexibleSpaceBar(
           centerTitle: false,
@@ -70,6 +56,36 @@ class _MoviePageState extends State<MoviePage> {
       ),
       SliverList(delegate: _sliver())
     ]);
+  }
+
+  /**
+   * Icone de favoritos com o Flare.
+   * O parâmetro animation recebe qual animação deve fazer.
+   * Lá no flare podemos ver que esse arquivo possui as animações Favorite e Unfavorite
+   * https://www.2dimensions.com/a/funwithflutter/files/flare/heart#
+   */
+  iconFavorito() {
+    return InkWell(
+          onTap: () {
+            _onClickFavoritar();
+          },
+          child: Container(
+            margin: EdgeInsets.only(right: 10),
+            width: 36,
+            height: 36,
+            child: StreamBuilder(
+                initialData: false,
+                stream: _bloc.getFavoritos,
+                builder: (context, snapshot) {
+                  return FlareActor(
+                    "assets/animations/favorite.flr",
+                    color: snapshot.data ? Colors.red : Colors.white,
+                    shouldClip: false,
+                    animation: snapshot.data ? "Favorite" : "Unfavorite",
+                  );
+                }),
+          ),
+        );
   }
 
   _sliver() {
